fd7b7de84bab89245ca2a56e5f4fd37ba5dfec78
gac05 <zuziagach@gmail.com>
2026-01-09 10:08:26 +0100

    Fix formatting and include statements in Zloz_zamowienie.cpp

---
+++ Zloz_zamowienie.cpp
@@ -0,0 +1,20 @@
+#include <exception>
+#include <string>
+using namespace std;
+
+#include "Zloz_zamowienie.h"
+#include "Zamowienie.h"
+
+void Zloz_zamowienie::uzupelnijDane() {
+	throw "Not yet implemented";
+}
+
+void Zloz_zamowienie::przejdzDoPlatnosci() {
+	throw "Not yet implemented";
+}
+
+IStrategiaPlatnosci Zloz_zamowienie::wybierzPłatnosc() {
+	throw "Not yet implemented";
+}
+
+
