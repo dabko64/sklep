38c0ceafcb4fd96db6b2d48f65039a26d3e4c52a
Natalia Dąbkowicz <dabkowicznatalia@gmail.com>
2026-01-09 10:06:50 +0100

    Update and rename Przeglądanie_produktów.cpp to Przegladanie_produktow.cpp

---
+++ Przegladanie_produktow.cpp
@@ -0,0 +1,17 @@
+#include <exception>
+using namespace std;
+
+#include "Przegladanie_produktow.h"
+void Przegladanie_produktow::dodajDoKoszyka() {
+	throw "Not yet implemented";
+}
+
+void Przegladanie_produktow::wyszukajProdukt() {
+	throw "Not yet implemented";
+}
+
+void Przegladanie_produktow::przejdzDoKoszyka() {
+	throw "Not yet implemented";
+}
+
+
