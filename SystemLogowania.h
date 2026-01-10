4cd64f2039ffb68dd3cf219497376f489f9b6b41
Natalia Dąbkowicz <dabkowicznatalia@gmail.com>
2026-01-11 00:16:49 +0100

    Update SystemLogowania.h

--- SystemLogowania.h
+++ SystemLogowania.h
@@ -1,29 +1,67 @@
-#include <exception>
+#ifndef SYSTEMLOGOWANIA_H
+#define SYSTEMLOGOWANIA_H
+
 #include <string>
-using namespace std;
-
-#ifndef __SystemLogowania_h__
-#define __SystemLogowania_h__
-
-// #include "Klient.h"
 
 class Klient;
-class SystemLogowania;
 
-class SystemLogowania
-{
-	private: string _dane;
-	private: string _haslo;
-	public: Klient* _unnamed_Klient_;
+class SystemLogowania {
+public:
+    //menu glowne: rejestracja lub logowanie
+    Klient* menu();
 
-	public: void sprawdzPoprawnosc();
+private:
+    //rejestracja nowego klienta (zapis do pliku)
+    void rejestracja();
 
-	public: void przejdzDoKonta();
+    //logowanie istniejacego klienta (odczyt z pliku)
+    Klient* logowanie();
 
-	public: void zatwierdzKonto();
-
-	public: void autoryzacja();
+    //sprawdzenie danych w pliku
+    bool sprawdzWLinii(const std::string& linia,
+                       const std::string& login,
+                       const std::string& haslo,
+                       Klient& klientOut);
 };
 
 #endif
+
+
+
+
+
+
+
+
+
+
+// #include <exception>
+// #include <string>
+// using namespace std;
+
+// #ifndef __SystemLogowania_h__
+// #define __SystemLogowania_h__
+
+// // #include "Klient.h"
+
+// class Klient;
+// class SystemLogowania;
+
+// class SystemLogowania
+// {
+// 	private: string _dane;
+// 	private: string _haslo;
+// 	public: Klient* _unnamed_Klient_;
+
+// 	public: void sprawdzPoprawnosc();
+
+// 	public: void przejdzDoKonta();
+
+// 	public: void zatwierdzKonto();
+
+// 	public: void autoryzacja();
+// };
+
+// #endif
+
 
