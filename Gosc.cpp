33dededc6f02f61e791eb9d718e3e5882115413c
Zuzanna Gach <zuziagach@gmail.com>
2026-01-12 23:24:33 +0100

    Implement account creation methods in Gosc class

--- Gosc.cpp
+++ Gosc.cpp
@@ -6,24 +6,59 @@ using namespace std;
 #include "Promocje.h"
 #include "IObserwatorPromocji.h"
 
+#include <fstream>
+
+// konstruktor – ustawia wartosci poczatkowe
+Gosc::Gosc()
+    : akceptacjaCookies(false) {}
+
+	// glowna metoda tworzenia konta
 void Gosc::stworzKonto() {
-	throw "Not yet implemented";
+	wybierzLogin();
+    wybierzHaslo();
+ // otwarcie pliku do zapisu (dopisywanie)
+    ofstream plik("klienci.txt", ios::app);
+    if (!plik) {
+        cout << "Blad zapisu do pliku\n";
+        return;
+    }
+  // zapis danych do pliku
+    plik << login << ";" << haslo << endl;
+    cout << "Konto utworzone poprawnie\n";
 }
-
+// metoda pobiera haslo od uzytkownika
 void Gosc::wybierzHaslo() {
-	throw "Not yet implemented";
+	cout << "Podaj haslo: ";
+    cin >> haslo;
 }
-
+// metoda pobiera login od uzytkownika
 void Gosc::wybierzLogin() {
-	throw "Not yet implemented";
+	cout << "Podaj login: ";
+    cin >> login;
+// sprawdzenie czy login juz istnieje
+    if (czyIstniejeTakiLogin(login)) {
+        cout << "Login juz istnieje. Wybierz inny.\n";
+        wybierzLogin(); // ponowne wpisanie loginu
+    }
 }
 
-void Gosc::czyIstniejeTakiLogin() {
-	throw "Not yet implemented";
+// sprawdzenie czy login znajduje sie w pliku klienci.txt
+bool Gosc::czyIstniejeTakiLogin(const string& login) const {
+	ifstream plik("klienci.txt");
+    if (!plik) return false;// jezeli plik nie istnieje, login na pewno jest wolny
+    string linia;
+	// czytanie pliku linia po linii
+    while (getline(plik, linia)) {
+		// sprawdzamy czy linia zaczyna sie od "login;"
+        if (linia.find(login + ";") == 0) {
+            return true;
+        }
+    }
+    return false;
 }
-
-void() Gosc::aktualizuj(Promocje aPromocja) {
-	throw "Not yet implemented";
+// metoda wywolywana po otrzymaniu promocji
+void Gosc::aktualizuj(Promocje aPromocja) {
+	  cout << "Gosc otrzymal promocje: " << aPromocja << endl;
 }
 
 
