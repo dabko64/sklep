4c33a70bebee75b6534093c9e98e67cfe85806f3
Zuzanna Gach <zuziagach@gmail.com>
2026-01-18 22:38:05 +0100

    Implement main function for user login and interaction in the store

--- main.cpp
+++ main.cpp
@@ -1,2 +1,27 @@
+#include "SystemLogowania.h"
+#include "Klient.h"
 #include <iostream>
 using namespace std;
+
+int main() {
+    SystemLogowania system;
+    Klient* aktualnyUzytkownik = nullptr;
+
+    cout << "--- WITAJ W SKLEPIE ---" <<endl;
+
+    // Pętla logowania/rejestracji
+    while (aktualnyUzytkownik == nullptr) {
+        aktualnyUzytkownik = system.menu();
+    }
+
+    // Tutaj użytkownik jest już zalogowany
+    aktualnyUzytkownik->zalogujSie();
+    
+    // Teraz możesz wywoływać metody Klienta
+    aktualnyUzytkownik->przejdzDoKoszyka();
+    
+    // Na koniec sprzątamy pamięć!
+    delete aktualnyUzytkownik;
+    
+    return 0;
+}
